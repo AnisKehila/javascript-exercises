127ec2dd15dfbf0d65928f6d0db4afea2b835b1b
Anis Kehila <aniskehila22@gmail.com>
2022-12-23 14:53:57 +0100

    Completing the exerices

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string , counter) {
+    if(counter < 0) {
+        return 'ERROR';
+    }
+    let newString = '';
+    for(let i = 0 ; i < counter ; i++) {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr , ...values) {
+    let arrayToReturn = [];
+    for(let i = 0 ; i < arr.length ; i++) {
+        if(!values.includes(arr[i])) {
+            arrayToReturn.push(arr[i]);
+        }
+    }
+    return arrayToReturn;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
